f28b98379acfe2f1fe39a8b585c16d9fa6437222
maroisa <ahmadroisul412@gmail.com>
2025-06-30 07:19:18 +0700

    add code blocks syntax highlight

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,10 @@
+import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
+
 export default async function(eleventyConfig) {
+    eleventyConfig.addPlugin(syntaxHighlight);
     eleventyConfig.setInputDirectory("views");
     eleventyConfig.addPassthroughCopy("assets/style.css");
+    eleventyConfig.addPassthroughCopy("assets/prism-twilight.css");
     eleventyConfig.addShortcode("navRight", (content, href) => {
         return `<a class="nav" href="${href}">${content} <span class="nav-arrow">-></span></a>`
     })
